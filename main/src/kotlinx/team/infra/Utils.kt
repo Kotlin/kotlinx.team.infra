0361e05d86678d3f2467ffa96cb98d2df61e08aa
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2023-09-01 05:14:52 +0200

    Make all helper utils internal

--- main/src/kotlinx/team/infra/Utils.kt
+++ main/src/kotlinx/team/infra/Utils.kt
@@ -1,13 +1,12 @@
 package kotlinx.team.infra
 
-import groovy.lang.*
 import org.gradle.api.*
 import org.gradle.api.logging.*
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.tasks.*
 import java.io.*
 
-fun cleanup(file: File) {
+internal fun cleanup(file: File) {
     if (file.exists()) {
         val listing = file.listFiles()
         if (listing != null) {
@@ -19,46 +18,27 @@ fun cleanup(file: File) {
     }
 }
 
-inline fun <reified T : Task> Project.task(
+internal inline fun <reified T : Task> Project.task(
     name: String,
-    depends: String? = null,
     noinline configuration: T.() -> Unit
-): TaskProvider<T> {
-    @Suppress("UnstableApiUsage")
-    val task = tasks.register(name, T::class.java, Action(configuration))
-    if (depends != null) {
-        tasks.getByName(depends).dependsOn(task)
-    }
-    return task
-}
+): TaskProvider<T> =
+    tasks.register(name, T::class.java, Action(configuration))
 
-class KotlinClosure1<T : Any?, V : Any>(
-    val function: T.() -> V?,
-    owner: Any? = null,
-    thisObject: Any? = null
-) : Closure<T>(owner, thisObject) {
-
-    @Suppress("unused") // to be called dynamically by Groovy
-    fun doCall(it: T): V? = it.function()
-}
-
-fun <T> Any.closureOf(action: T.() -> Unit) : Closure<T> =
-    KotlinClosure1<T, Unit>(action, this, this)
-
-fun <T> Project.tryGetClass(className: String): Class<T>? {
+internal fun <T> Project.tryGetClass(className: String): Class<T>? {
     val classLoader = buildscript.classLoader
     return try {
+        @Suppress("UNCHECKED_CAST")
         Class.forName(className, false, classLoader) as Class<T>
     } catch (e: ClassNotFoundException) {
         null
     }
 }
 
-fun Logger.infra(message: String) {
+internal fun Logger.infra(message: String) {
     info("INFRA: $message")
 }
 
-fun Project.propertyOrEnv(name: String): String? =
+internal fun Project.propertyOrEnv(name: String): String? =
     findProperty(name) as? String ?: System.getenv(name)
 
 internal inline fun <reified T: Any> ObjectFactory.newInstance(): T = newInstance(T::class.java)
