44fb9efdf6f98c7c731a0a961b2c446c405e6012
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2023-09-13 22:16:26 +0200

    Update TC DSL version used in TC configs

--- main/resources/teamcity/utils.kt
+++ main/resources/teamcity/utils.kt
@@ -3,7 +3,7 @@
  * Use of this source code is governed by the Apache 2.0 License that can be found in the LICENSE.txt file.
  */
 
-import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.*
 
 const val versionSuffixParameter = "versionSuffix"
 const val teamcitySuffixParameter = "teamcitySuffix"
